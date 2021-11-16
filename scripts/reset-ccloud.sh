df6ee8a0b6428670439be01ca7fda9b381b52a45
mark-christiansen <m_christiansen@comcast.net>
2021-11-15 20:56:04 -0600

    bootstrap flux-demo-ccloud-operator

--- scripts/reset-ccloud.sh
+++ scripts/reset-ccloud.sh
@@ -3,14 +3,14 @@
 ACCOUNT_NAME="flux-demo-producer"
 ACCOUNT_ID=$(ccloud service-account list | grep "$ACCOUNT_NAME" | grep -Eo '[0-9]{6}')
 
+#clear api-key for ccloud operator
+echo "deleting api-key for ccloud operator"
+API_KEY=$(ccloud api-key list | grep "cc.api-key.cloud" | grep -Eo '[A-Z0-9]{16}')
+ccloud api-key delete $API_KEY
+
 # clear service account
 echo "deleting service account $ACCOUNT_ID"
 ccloud service-account delete $ACCOUNT_ID
-
-# clear api-keys for service account
-#API_KEY=$(ccloud api-key list | grep "$ACCOUNT_ID" | grep -Eo '[A-Z0-9]{16}')
-#echo "deleting api-key $API_KEY"
-#ccloud api-key delete $API_KEY
 
 TOPICS=("source-topic-0" "source-topic-1" "source-topic-2")
 # clear acls and topics
