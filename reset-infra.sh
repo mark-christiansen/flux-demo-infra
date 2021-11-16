8ef12368357c8a67e8d0341b2848e20fb7179944
mark-christiansen <m_christiansen@comcast.net>
2021-11-15 19:34:05 -0600

    cleaning out infra

--- reset-infra.sh
+++ reset-infra.sh
@@ -6,3 +6,10 @@ rm clusters/my-cluster/*.yaml
 echo "commiting changes to flux-demo-infra repo"
 git add . && git commit -m "cleaning out infra" && git push
 
+NS="default"
+DEPLOYD=("ccloud-operator" "flux-demo-producer")
+
+echo "removing pods and deployments"
+for DEPLOY in ${DEPLOYS[@]}; do
+  kubectl -n $NS delete deployment $DEPLOY
+done
