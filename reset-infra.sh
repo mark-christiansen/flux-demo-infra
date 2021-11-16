b5680562171c8e2313618a238648337c3cdff4a9
mark-christiansen <m_christiansen@comcast.net>
2021-11-15 19:41:37 -0600

    cleaning out infra

--- reset-infra.sh
+++ reset-infra.sh
@@ -7,9 +7,23 @@ echo "commiting changes to flux-demo-infra repo"
 git add . && git commit -m "cleaning out infra" && git push
 
 NS="default"
-DEPLOYS=("ccloud-operator" "flux-demo-producer")
 
+DEPLOYS=("ccloud-operator" "flux-demo-producer")
 echo "removing pods and deployments"
 for DEPLOY in ${DEPLOYS[@]}; do
   kubectl -n $NS delete deployment $DEPLOY
 done
+
+CONFIGS=("" "")
+echo "removing configmaps"
+for CONFIG in ${CONFIGS[@]}; do
+  kubectl -n $NS delete configmap $CONFIG
+done
+
+SECRETS=("cc.api-key.cloud" "cc.api-key.flux-demo-producer.lkc-1pgrz" "cc.bootstrap-servers.default.devops-demo" "cc.sasl-jaas-config.flux-demo-producer.default.devops-demo " "cc.schema-registry-url.default")
+echo "removing secrets"
+for SECRET in ${SECRETS[@]}; do
+  kubectl -n $NS delete secret $SECRET
+done
+
+echo "finished reseting infra"
