8f3fdb500990dbab64ab6c94ff63da263c308e4c
mark-christiansen <m_christiansen@comcast.net>
2021-11-15 19:34:47 -0600

    cleaning out infra

--- reset-infra.sh
+++ reset-infra.sh
@@ -7,7 +7,7 @@ echo "commiting changes to flux-demo-infra repo"
 git add . && git commit -m "cleaning out infra" && git push
 
 NS="default"
-DEPLOYD=("ccloud-operator" "flux-demo-producer")
+DEPLOYS=("ccloud-operator" "flux-demo-producer")
 
 echo "removing pods and deployments"
 for DEPLOY in ${DEPLOYS[@]}; do
