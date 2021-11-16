2ca4b0f1acd36155ad5674f0460b027e3074ad9b
mark-christiansen <m_christiansen@comcast.net>
2021-11-15 19:50:41 -0600

    bootstrap flux-demo-ccloud-operator

---
+++ install-operator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAME=flux-demo-ccloud-operator
+REPO=https://github.com/mark-christiansen/$NAME
+POD=ccloud-operator
+
+flux create source git $POD --url=$REPO --branch=main --interval=30s \
+  --export > ./clusters/my-cluster/$NAME-source.yaml
+
+flux create kustomization $POD --target-namespace=$NS --source=$POD \
+  --path="./kustomize" --prune=true --interval=5m \
+  --export > ./clusters/my-cluster/$NAME-kustomization.yaml
+
+git add . && git commit -m "bootstrap $NAME" && git push
